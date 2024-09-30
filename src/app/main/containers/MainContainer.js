dc6a29aa73666c60c2aba9d7f5ad775cb7227527
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-30 11:34:18 +0900

    앵커 연동

--- src/app/main/containers/MainContainer.js
+++ src/app/main/containers/MainContainer.js
@@ -1,7 +1,25 @@
+'use client';
 import React from 'react';
+import { Section, SectionsContainer } from 'react-fullpage';
+
+const options = {
+  anchors: ['sectionOne', 'setionTwo', 'sectionThree'],
+};
 
 const MainContainer = () => {
-  return <></>;
+  return (
+    <SectionsContainer {...options}>
+      <Section>
+        <h1>이력서</h1>
+      </Section>
+      <Section>
+        <h1>Education & Work Experience</h1>
+      </Section>
+      <Section>
+        <h1>Portfolio</h1>
+      </Section>
+    </SectionsContainer>
+  );
 };
 
 export default React.memo(MainContainer);
