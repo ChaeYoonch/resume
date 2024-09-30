6f8722321d2377f66ea9fbbd747e182d14e7de51
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-30 14:14:57 +0900

    profile 수정

--- src/app/main/components/ProfileImage.js
+++ src/app/main/components/ProfileImage.js
@@ -2,13 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProfileImage = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <div className={className}>
+      <div className="layout-width">
+        <div className="tit">
+          <span>RESUME</span>
+          정채윤
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const StyledProfileImage = styled(ProfileImage)`
   background: #dbdbdb;
   height: 350px;
   padding: 50px 30px 10px;
+  position: relative;
+  .tit {
+    position: absolute;
+    bottom: 10px;
+    font-size: 1.15rem;
+
+    span {
+      font-size: 1.75rem;
+      margin-right: 10px;
+    }
+  }
 `;
 
 export default React.memo(StyledProfileImage);
