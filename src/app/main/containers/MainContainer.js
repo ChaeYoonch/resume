d636a9ab9049c87bd7f107e281071ba984f98e7f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-30 11:39:18 +0900

    링크 연동

--- src/app/main/containers/MainContainer.js
+++ src/app/main/containers/MainContainer.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Section, SectionsContainer } from 'react-fullpage';
+import { Section, SectionsContainer, Header } from 'react-fullpage';
 
 const options = {
   anchors: ['sectionOne', 'setionTwo', 'sectionThree'],
@@ -8,17 +8,24 @@ const options = {
 
 const MainContainer = () => {
   return (
-    <SectionsContainer {...options}>
-      <Section>
-        <h1>이력서</h1>
-      </Section>
-      <Section>
-        <h1>Education & Work Experience</h1>
-      </Section>
-      <Section>
-        <h1>Portfolio</h1>
-      </Section>
-    </SectionsContainer>
+    <>
+      <Header>
+        <a href="#sectionOne">INFO & ABOUT</a>
+        <a href="#sectionTwo">EXPERIENCE</a>
+        <a href="#sectionThree">PORTFOLIO</a>
+      </Header>
+      <SectionsContainer {...options}>
+        <Section>
+          <h1>이력서</h1>
+        </Section>
+        <Section>
+          <h1>Education & Work Experience</h1>
+        </Section>
+        <Section>
+          <h1>Portfolio</h1>
+        </Section>
+      </SectionsContainer>
+    </>
   );
 };
 
