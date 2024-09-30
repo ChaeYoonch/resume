796758d567701a8a64971c8e56f686e2d4196e40
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-09-30 12:33:13 +0900

    메뉴바 디자인 수정

--- src/app/main/components/TopMenu.js
+++ src/app/main/components/TopMenu.js
@@ -1,12 +1,29 @@
+'use client';
 import React from 'react';
 import { Header } from 'react-fullpage';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  background: #fff;
+  border-bottom: 1px solid #000;
+  a {
+    line-height: 49px;
+    padding: 0 30px;
+    font-sixe: 1.75rem;
+  }
+`;
 
 const TopMenu = () => {
   return (
     <Header>
-      <a href="#sectionOne">INFO & ABOUT</a>
-      <a href="#sectionTwo">EXPERIENCE</a>
-      <a href="#sectionThree">PORTFOLIO</a>
+      <StyledNav>
+        <a href="#sectionOne">INFO & ABOUT</a>
+        <a href="#sectionTwo">EXPERIENCE</a>
+        <a href="#sectionThree">PORTFOLIO</a>
+      </StyledNav>
     </Header>
   );
 };
